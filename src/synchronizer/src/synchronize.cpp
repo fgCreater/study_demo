2c745137a80607ccd0cc4d097804a9e107f46fec
ff <1316664178@qq.com>
2024-12-16 22:25:41 -0500

    多个话题时间戳同步

--- src/synchronizer/src/synchronize.cpp
+++ src/synchronizer/src/synchronize.cpp
@@ -13,8 +13,7 @@
 #include <sensor_msgs/PointCloud2.h>
 using namespace std;
 
-
-void callback(const sensor_msgs::PointCloud2::ConstPtr &points_01,const sensor_msgs::PointCloud2::ConstPtr &points_02){
+void callback(const sensor_msgs::PointCloud2::ConstPtr &points_01,const sensor_msgs::PointCloud2::ConstPtr &points_02,const sensor_msgs::PointCloud2::ConstPtr &points_03){
     ROS_INFO("时间同步u");
 }
 int main(int argc, char *argv[])
@@ -23,16 +22,18 @@ int main(int argc, char *argv[])
     //执行节点初始化
     ros::init(argc,argv,"HelloVSCode");
     ros::NodeHandle nh;
+    std::cout << "Hello, world!" << endl;
     // 1.创建接受消息对象
      message_filters::Subscriber<sensor_msgs::PointCloud2>points_01(nh,"filed_points_01",10);
      message_filters::Subscriber<sensor_msgs::PointCloud2>points_02(nh,"filted_points_02",10);
+    message_filters::Subscriber<sensor_msgs::PointCloud2>points_03(nh,"filted_points_03",10);
      //2. message_filters::Cache 对象来管理接收到的消息
-    typedef message_filters::sync_policies::ExactTime<sensor_msgs::PointCloud2,sensor_msgs::PointCloud2>MySyncPolicy;
+    typedef message_filters::sync_policies::ExactTime<sensor_msgs::PointCloud2,sensor_msgs::PointCloud2,sensor_msgs::PointCloud2>MySyncPolicy;
     // typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2,sensor_msgs::PointCloud2>MySyncPolicy2;
     // 3.使用 message_filters::Synchronizer 类将订阅者、缓存和回调函数组合在一起，并设置同步的时间窗口大小等参数
-    message_filters:: Synchronizer<MySyncPolicy>sync (MySyncPolicy(10),points_01,points_02);
+    message_filters:: Synchronizer<MySyncPolicy>sync (MySyncPolicy(10),points_01,points_02,points_03);
     // 4.调用 Synchronizer 对象的 registerCallback() 函数来注册回调函数。
-    sync.registerCallback(boost::bind(&callback,_1,_2));
+    sync.registerCallback(boost::bind(&callback,_1,_2,_3));
     ros::spin();
     //输出日志
     ROS_INFO("Hello VSCode!!!哈哈哈哈   哈哈哈哈哈哈");
